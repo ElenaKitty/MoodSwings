1214961c31f6661dfe6a99f5c682ac8c8cc800e5
Bodhi Duijvekam <bodhiduijvekam@gmail.com>
2019-11-27 10:17:07 +0100

    Spotify API Code v.0.1
    First version of the API code

---
+++ MusicAlgorithm/MusicAlgorithm/SpotifyAPI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+namespace MusicAlgorithm
+{
+    class SpotifyAPI
+    {
+        private String OAuth;
+        private String url = "https://api.spotify.com/v1/me/player/currently-playing?market=NL";
+
+        public SpotifyAPI(String OAuth)
+        {
+            this.OAuth = OAuth;
+        }
+
+        public async void getRequest(String url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", OAuth);
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        String myContent = await content.ReadAsStringAsync();
+                        using (StreamWriter file = File.CreateText(@"C:\Users\bodhi\source\repos\MusicAlgorithm\MusicAlgorithm\Resources\song.json"))
+                        {
+                            if (myContent != "")
+                            {
+                                JObject json = JObject.Parse(myContent);
+                                dynamic jsondata = json;
+                                object name = jsondata.item.album.artists[0].name;
+                                Console.WriteLine("Hiero is dat naampie " + name);
+                                JsonSerializer serializer = new JsonSerializer();
+                                serializer.Serialize(file, json);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
