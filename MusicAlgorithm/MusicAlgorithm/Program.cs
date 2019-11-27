27456d432b525ffc156a1070d63d389778721361
Bodhi Duijvekam <bodhiduijvekam@gmail.com>
2019-11-27 10:17:07 +0100

    Spotify API Code
    First version of the API code

---
+++ MusicAlgorithm/MusicAlgorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MusicAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpotifyAPI spotify = new SpotifyAPI("BQDA7RGEeZBnOUeJt53mdKX44P0VUuLPh8vhMKTJgkjKV74lyI29VqRmHu9HjSrRpMTZA2Xdn9JXn4_R2dv8awCkTdpGpl4Ty-AuxL70WyVfY0bUv1a1FDzfSasG78puCGitO-q2xiDF2JJ7eY5XgOHb8K0Ogj7SwA");
+
+            String input = "";
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                if (input == "try")
+                {
+                    spotify.getRequest("https://api.spotify.com/v1/me/player/currently-playing?market=NL");
+                }
+            }
+        }
+    }
+}
---
+++ MusicAlgorithm/MusicAlgorithm/Song.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicAlgorithm
+{
+    class Song
+    {
+        int duration_ms;
+        float danceability;
+        float energy;
+        float tempo;
+
+        public Song(int duration, float danceability, float energy, float tempo)
+        {
+            this.duration_ms = duration;
+            this.danceability = danceability;
+            this.energy = energy;
+            this.tempo = tempo;
+        }
+
+        public int Duration_ms 
+        { 
+            get { return duration_ms; }
+            set { duration_ms = value; } 
+        }
+
+        public float Danceability
+        {
+            get { return danceability; }
+            set { danceability = value; }
+        }
+
+        public float Energy
+        {
+            get { return energy; }
+            set { energy = value; }
+        }
+
+        public float Tempo
+        {
+            get { return tempo; }
+            set { tempo = value; }
+        }
+    }
+}
