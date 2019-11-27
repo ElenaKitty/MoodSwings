71fbf598bd094a1032070ed961b8921965f856b2
Bodhi Duijvekam <bodhiduijvekam@gmail.com>
2019-11-27 15:18:58 +0100

    Spotify API Code v.0.2
    Now you can get the current track, get it's features and it's analysis

--- MusicAlgorithm/MusicAlgorithm/Song.cs
+++ MusicAlgorithm/MusicAlgorithm/Song.cs
@@ -6,23 +6,19 @@ namespace MusicAlgorithm
 {
     class Song
     {
-        int duration_ms;
+        String songName;
+        String artistName;
         float danceability;
         float energy;
         float tempo;
 
-        public Song(int duration, float danceability, float energy, float tempo)
+        public Song(String songName, String artistName, float danceability, float energy, float tempo)
         {
-            this.duration_ms = duration;
+            this.songName = songName;
+            this.artistName = artistName;
             this.danceability = danceability;
             this.energy = energy;
             this.tempo = tempo;
-        }
-
-        public int Duration_ms 
-        { 
-            get { return duration_ms; }
-            set { duration_ms = value; } 
         }
 
         public float Danceability
